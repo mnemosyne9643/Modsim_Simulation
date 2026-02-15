170540317ccc74cce712089d3128f526b480704e
Mnemosyne9643 <zoiegracedepaz04@gmail.com>
2026-02-15 21:24:11 +0800

    add css

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.Web.WebView2;
+using Microsoft.Web.WebView2.Core;
 using System.Text.Json;
 namespace Modsim_Simulation
 {
@@ -8,10 +9,27 @@ namespace Modsim_Simulation
         public Form1()
         {
             InitializeComponent();
-            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-            string fullPath = Path.Combine(baseDir, "FrontEnd", "index.html");
-            webViewForms.Source = new Uri(fullPath);
+            //string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            //string fullPath = Path.Combine(baseDir, "FrontEnd", "index.html");
+            //webViewForms.Source = new Uri(fullPath);
+
+            InitializeWebView();
+
         }
 
+        async void InitializeWebView()
+        {
+            await webViewForms.EnsureCoreWebView2Async();
+
+            string rootFolder = Path.Combine(Application.StartupPath, "FrontEnd");
+
+            webViewForms.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                "app.local",
+                rootFolder,
+                CoreWebView2HostResourceAccessKind.Allow
+                );
+
+            webViewForms.CoreWebView2.Navigate("https://app.local/index.html");
+        }
     }
 }
